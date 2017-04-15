0ebc9a3bed7e8a7b85d91f861344fe2306ab6114
James Clancey <james.clancey@gmail.com>
2017-04-14 16:30:53 -0800

    Now cleanse search inputs!

--- MusicPlayer.Shared/ViewModels/LocalSearchListViewModel.cs
+++ MusicPlayer.Shared/ViewModels/LocalSearchListViewModel.cs
@@ -12,38 +12,34 @@ namespace MusicPlayer.ViewModels
 	    string searchString;
 	    public async void Search(string query)
 	    {
-		    searchString = query;
+			searchString = $"%{query}%";
 		    Results = null;
 		    var result = new SearchResults();
 		    result.Songs =
 			    await
-				    Database.Main.QueryAsync<Song>("select * from song where " +
-													string.Format("Name like ('%{0}%') or Artist  like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
+				    Database.Main.QueryAsync<Song>("select * from song where Name like (?) or Artist  like (?)" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString,searchString);
 			result.Artist =
-					await Database.Main.QueryAsync<Artist>("select * from Artist where " +
-													string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
+					await Database.Main.QueryAsync<Artist>("select * from Artist where Name like (?)" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString);
 			result.Albums =
-					await Database.Main.QueryAsync<Album>("select * from Album where " +
-													string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
+					await Database.Main.QueryAsync<Album>("select * from Album where Name like (?)" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString);
 			result.Playlists =
-					await Database.Main.QueryAsync<Playlist>("select * from Playlist where " +
-													string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
+					await Database.Main.QueryAsync<Playlist>("select * from Playlist where Name like (?)" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString);
 			Results = result;
 	    }
 
 		public GroupInfo GetArtist()
 		{
-			return new GroupInfo() { Filter = string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString) };
+			return new GroupInfo() { Filter = "Name like (@SearchString)" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), Params = { { "@SearchString", "searchString" } } };
 		}
 
 		public GroupInfo GetAlbum()
 		{
-			return new GroupInfo() { Filter = string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString) };
+			return new GroupInfo() { Filter = "Name like (@SearchString)" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), Params = { { "@SearchString", "searchString" } }  };
 		}
 
 		public GroupInfo GetSongs()
 		{
-			return new GroupInfo() { Filter = string.Format("Title like ('%{0}%') or Artist  like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and IsLocal = 1" : ""), searchString) };
+			return new GroupInfo() { Filter ="Title like (@SearchString) or Artist  like (@SearchString)" + ((Settings.ShowOfflineOnly) ? " and IsLocal = 1" : ""), Params = { { "@SearchString", "searchString" } }  };
 		}
 	}
 }
