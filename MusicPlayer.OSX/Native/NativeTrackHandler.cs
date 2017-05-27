f9fe20fbf9de8fb6142d5900cfbf684c7d84d0bf
James Clancey <james.clancey@gmail.com>
2017-05-26 19:42:33 -0700

    OS X now Does Notifications.

--- MusicPlayer.OSX/Native/NativeTrackHandler.cs
+++ MusicPlayer.OSX/Native/NativeTrackHandler.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 using MusicPlayer.Managers;
+using Foundation;
+using MusicPlayer.Models;
+using AppKit;
+using System.Threading.Tasks;
+using Akavache;
+using System.Reactive.Linq;
+using Splat;
 
 namespace MusicPlayer
 {
@@ -12,6 +19,95 @@ namespace MusicPlayer
 		public void Init()
 		{
 
+			NotificationManager.Shared.CurrentSongChanged += (sender, args) => UpdateSong(args.Data);
+			NotificationManager.Shared.PlaybackStateChanged += (sender, e) => PlaybackStateChanged (e.Data);
+			NSUserNotificationCenter.DefaultUserNotificationCenter.DidActivateNotification += (object sender, UNCDidActivateNotificationEventArgs e) => {
+				switch (e.Notification.ActivationType)
+				{
+        			case NSUserNotificationActivationType.ActionButtonClicked:
+					PlaybackManager.Shared.NextTrack ();
+					break;
+				}
+			};
+
+			//NotificationManager.Shared.CurrentTrackPositionChanged += (sender, args) => UpdateProgress(args.Data);
+		}
+
+		public void UpdateSong (Song song)
+		{
+			var frontmost = NSWorkspace.SharedWorkspace.FrontmostApplication.BundleIdentifier == NSBundle.MainBundle.BundleIdentifier;
+			if (frontmost || PlaybackManager.Shared.NativePlayer.State != PlaybackState.Playing)
+				return;
+			var notification = CreateNotification (song);
+			NSUserNotificationCenter.DefaultUserNotificationCenter.RemoveAllDeliveredNotifications ();
+			NSUserNotificationCenter.DefaultUserNotificationCenter.DeliverNotification (notification);
+		}
+
+		public void PlaybackStateChanged (PlaybackState state )
+		{
+			if (state == PlaybackState.Paused || state == PlaybackState.Stopped)
+				NSUserNotificationCenter.DefaultUserNotificationCenter.RemoveAllDeliveredNotifications ();
+			if (state == PlaybackState.Playing)
+				UpdateSong (PlaybackManager.Shared.NativePlayer.CurrentSong);
+		}
+
+		public NSUserNotification CreateNotification (Song song)
+		{
+			var notification = new NSUserNotification {
+				Title = song.Name,
+				Subtitle = song.Album,
+				InformativeText = song.Artist,
+				HasActionButton = true,
+				ActionButtonTitle = "Skip",
+			};
+
+			//todo: get artwork
+			//notification.ContentImage
+			SetImage (notification, song);
+			//notification.contentImage = [self albumArtForTrack: currentTrack];
+
+
+			//Private APIs – remove if publishing to Mac App Store
+			try {
+				notification.SetValueForKey (NSObject.FromObject (true), (NSString)"_showsButtons");
+				// Show album art on the left side of the notification (where app icon normally is),
+				//like iTunes does
+				//[notification setValue:notification.contentImage forKey:@"_identityImage"];
+				//notification.contentImage = nil;
+			} catch (Exception ex) {
+				Console.WriteLine (ex);	
+			}
+
+    		return notification;
+		}
+		async void SetImage (NSUserNotification notification, Song song)
+		{
+
+		}
+		async Task<NSImage> GetImage (MediaItemBase item, NSImage defaultImage = null)
+		{
+			float imageWidth = 60f;
+			try {
+				if (defaultImage == null)
+					defaultImage = Images.GetDefaultAlbumArt (imageWidth);
+				if (item == null)
+					return defaultImage;
+				
+				var image = await item.GetLocalImage (imageWidth);
+				if (image != null) {
+					return image;
+				} else {
+					var artUrl = await ArtworkManager.Shared.GetArtwork (item);
+
+					if (string.IsNullOrWhiteSpace (artUrl))
+						return defaultImage;
+					var bitmap = (await BlobCache.LocalMachine.LoadImageFromUrl (artUrl, desiredWidth: imageWidth));
+					return bitmap.ToNative ();
+				}
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
+			return defaultImage;
 		}
 	}
 }
