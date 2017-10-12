7e3b865420e5f16928c9e44f943af00f5b149799
James Clancey <james.clancey@gmail.com>
2017-10-11 21:56:15 -0800

    TRY 4

--- mobile-center-post-clone.sh
+++ mobile-center-post-clone.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 sh ./Setup.sh
-$(Agent.HomeDirectory)/scripts/select-xamarin-sdk.sh Stable
---
+++ mobile-center-pre-build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+$(Agent.HomeDirectory)/scripts/select-xamarin-sdk.sh Stable
