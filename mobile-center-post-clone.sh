cea214d8fa773a78d8009cfea317fa5886461d3e
James Clancey <james.clancey@gmail.com>
2017-11-03 09:44:16 -0800

    Added appStore Setup

--- mobile-center-post-clone.sh
+++ mobile-center-post-clone.sh
@@ -5,4 +5,15 @@ else
     echo 'Downloading ApiConstants.cs'
     curl -o ApiConstants.cs "${ApiConstantsUrl}"
 fi
+
+if [[ ${MOBILECENTER_BRANCH} = "AppStoreRelease" ]]
+then
+    echo 'App Store Build'
+    cd MusicPlayer.iOS
+    sh ./SetupAppStore.sh
+    cd ..
+else
+    echo 'Normal Branch'
+fi
+
 sh ./Setup.sh
