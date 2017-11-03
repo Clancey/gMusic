fa74843ee6877b10c72783f42e104c910ed0f61c
James Clancey <james.clancey@gmail.com>
2017-11-03 09:37:02 -0800

    Added ApiConstants download for mobile center

--- mobile-center-post-clone.sh
+++ mobile-center-post-clone.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+if [[ -z "${ApiConstantsUrl}" ]]; then
+    echo 'No ApiConstantsUrl'
+else
+    echo 'Downloading ApiConstants.cs's
+    curl -o ApiConstants.cs "${ApiConstantsUrl}"
+fi
 sh ./Setup.sh
