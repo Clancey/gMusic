5c44351c2e4797c4a98456d4ffa6e99fc9c204cc
James Clancey <james.clancey@gmail.com>
2017-11-06 12:00:35 -0900

    Updated code signing

--- MusicPlayer.iOS/SetupDev.sh
+++ MusicPlayer.iOS/SetupDev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier com.IIS.MusicPlayer.iOS" Info.plist
-/usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName gMusic beta" Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName gMusic Beta" Info.plist
