684d4486158bf35974c0a251fb7c33cccbd715c7
James Clancey <james.clancey@gmail.com>
2017-12-18 12:34:46 -0900

    Fixed app store scripts

--- MusicPlayer.iOS/SetupAppStore.sh
+++ MusicPlayer.iOS/SetupAppStore.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo 'Set Bundle ID to com.youriisolutions.gMusic2'
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier com.youriisolutions.gMusic2" Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName gMusic" Info.plist
