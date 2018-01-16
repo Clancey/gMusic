e5f1bf05a8ee7bce205890ead947173fc1bb02f3
James Clancey <james.clancey@gmail.com>
2018-01-16 09:08:59 -0900

    Added some try catches to the local search

--- MusicPlayer.Shared/ViewModels/LocalSearchListViewModel.cs
+++ MusicPlayer.Shared/ViewModels/LocalSearchListViewModel.cs
@@ -4,32 +4,68 @@ using System.Text;
 using MusicPlayer.Data;
 using MusicPlayer.Models;
 using SimpleDatabase;
+using MusicPlayer.Managers;
 
 namespace MusicPlayer.ViewModels
 {
-    class LocalSearchListViewModel : SearchListViewModel
-    {
-	    string searchString;
-	    public async void Search(string query)
-	    {
-		    searchString = query;
-		    Results = null;
-		    var result = new SearchResults();
-		    result.Songs =
-			    await
-				    Database.Main.QueryAsync<Song>("select * from song where " +
-													string.Format("Name like ('%{0}%') or Artist  like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
-			result.Artist =
-					await Database.Main.QueryAsync<Artist>("select * from Artist where " +
-													string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
-			result.Albums =
-					await Database.Main.QueryAsync<Album>("select * from Album where " +
-													string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
-			result.Playlists =
+	class LocalSearchListViewModel : SearchListViewModel
+	{
+		string searchString;
+		public async void Search(string query)
+		{
+			searchString = query;
+			Results = null;
+			var result = new SearchResults();
+			if (string.IsNullOrWhiteSpace(query))
+			{
+				Results = result;
+				return;
+			}
+			try
+			{
+				result.Songs =
+					await
+						Database.Main.QueryAsync<Song>("select * from song where " +
+														string.Format("Name like ('%{0}%') or Artist  like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
+			}
+			catch (Exception ex)
+			{
+				LogManager.Shared.Report(ex);
+			}
+			try
+			{
+				result.Artist =
+						await Database.Main.QueryAsync<Artist>("select * from Artist where " +
+														string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
+			}
+			catch (Exception ex)
+			{
+				LogManager.Shared.Report(ex);
+			}
+
+			try
+			{
+				result.Albums =
+						await Database.Main.QueryAsync<Album>("select * from Album where " +
+														string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
+			}
+			catch (Exception ex)
+			{
+				LogManager.Shared.Report(ex);
+			}
+
+			try
+			{
+				result.Playlists =
 					await Database.Main.QueryAsync<Playlist>("select * from Playlist where " +
 													string.Format("Name like ('%{0}%')" + ((Settings.ShowOfflineOnly) ? " and OfflineCount > 0" : ""), searchString));
+			}
+			catch (Exception ex)
+			{
+				LogManager.Shared.Report(ex);
+			}
 			Results = result;
-	    }
+		}
 
 		public GroupInfo GetArtist()
 		{
