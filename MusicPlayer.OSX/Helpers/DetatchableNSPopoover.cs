1e6cd2fdf8de606d230bea8b3a7d0d3b0e76c84c
James Clancey <james.clancey@gmail.com>
2018-03-15 13:18:35 -0800

    Fixed MacOS build

---
+++ MusicPlayer.OSX/Helpers/DetatchableNSPopoover.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AppKit;
+using Foundation;
+namespace MusicPlayer
+{
+	public class DetatchableNSPopoover : NSPopover, INSPopoverDelegate
+	{
+		public DetatchableNSPopoover()
+		{
+			this.Delegate = this;
+		}
+
+		[ExportAttribute("popoverShouldDetach:")]
+		public bool ShouldDetach(NSPopover popover) => true;
+	}
+
+}
